92b4314d5fe07d86ce94aaf854b7e32e8fb68ceb
Jessica Nelson <jessica.nelson60@gmail.com>
2023-10-12 23:19:52 -0600

    added objects

--- client.js
+++ client.js
@@ -4,3 +4,45 @@ const goodCharacters = ["Harry Potter", "Ron Weasley", "Hermione Granger", "Dobb
 const badCharacters = ["Voldemort", "Bellatrix Lestrange", "Peter Pettigrew", "Dolores Umbridge"];
 
 
+// Objects
+const harryPotter = {
+  name: "Harry Potter",
+  house: "Gryffindor",
+  halfBlood: "True"
+}
+
+const ronWeasley = {
+  name: "Ron Weasley",
+  house: "Gryffindor",
+  halfBlood: "False"
+}
+
+const hermioneGranger = {
+  name: "Hermione Granger",
+  house: "Gryffindor",
+  halfBlood: "False"
+}
+
+const dobby = {
+  name: "Dobby",
+  house: "None",
+  halfBlood: "False"
+}
+
+const severusSnape = {
+  name: "Severus Snape",
+  house: "Slytherin",
+  halfBlood: "True"
+}
+
+const albusDumbledore = {
+  name: "Albus Dumbledore",
+  house: "Gryffindor",
+  halfBlood: "True"
+}
+
+const siriusBlack = {
+  name: " Sirius Black",
+  house: "Gryffindor",
+  halfBlood: "False"
+}

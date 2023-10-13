28ec99151b65a53f09c356343f5c6b0d5db55cdc
Jessica Nelson <jessica.nelson60@gmail.com>
2023-10-12 23:19:00 -0600

    set up array's

--- client.js
+++ client.js
@@ -1,1 +1,6 @@
+// Array #1
+const goodCharacters = ["Harry Potter", "Ron Weasley", "Hermione Granger", "Dobby", "Severus Snape", " Albus Dumbledore", "Sirius Black"];
+// Array #2
+const badCharacters = ["Voldemort", "Bellatrix Lestrange", "Peter Pettigrew", "Dolores Umbridge"];
+
 

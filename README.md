623acd08dbe57b2320a8202c350942f29d2610a4
Jessica Nelson <jessica.nelson60@gmail.com>
2023-10-13 21:54:43 -0600

    rendered elements and updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # CPNT 262 Assignment 3: Objects and Arrays
 Author: Jessica Nelson
+
+## Attributions
+- [MDN Web Docs](https://developer.mozilla.org/en-US/)
+- [W3Schools](https://www.w3schools.com/)
+- Used these websites to find and better explain the array methods and object methods.
+
+## Code Journal
+- Used MDN Web Docs and W3Schools to find and better explain the array methods and object methods.
+
+## Practical Use Cases for Code
+- push()
+  - Push () can be a great technique to expand arrays when it needs to dynamically grow as new data is received.
+- pop()
+  - pop () is an easy technique to get rid of the last element in an array. When you no longer require a component of the array and want to shorten it, this can be useful.
+- concat() 
+  - Concat () is great as it makes code more concise if you combine numerous arrays or values in a single line.
+- Object.values()
+  - Using Object.values() can be useful as it makes it simple to extract and display property values while debugging or logging objects.
+- Object.keys()
+  - When determining whether an object contains a particular property, use Object.keys().
+- Object.freeze()
+  - This can be useful to prevent any unwanted modifications to certain objects. It can be essential in some systems that require high levels of security. These Objects are locked down to avoid tampering when they are frozen.
--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 // Array #1
-const goodCharacters = ["Harry Potter", "Ron Weasley", "Hermione Granger", "Dobby", "Severus Snape", " Albus Dumbledore", "Sirius Black"];
+const goodCharacters = ["Harry Potter", "Ron Weasley", "Hermione Granger", "Dobby", "Severus Snape", "Albus Dumbledore", "Sirius Black"];
 // Array #2
 const badCharacters = ["Voldemort", "Bellatrix Lestrange", "Peter Pettigrew", "Dolores Umbridge"];
 
@@ -41,7 +41,7 @@ const albusDumbledore = {
 }
 
 const siriusBlack = {
-  name: " Sirius Black",
+  name: "Sirius Black",
   house: "Gryffindor",
   halfBlood: "False"
 }
@@ -50,26 +50,32 @@ const siriusBlack = {
 
 // push() - Adds item(s) to the end of an array
 goodCharacters.push("Neville Longbottom") 
-document.getElementById("array-push").innerHTML = goodCharacters;
+document.getElementById("push").innerHTML = goodCharacters;
 
 // pop() - Removes the last item from an array
 goodCharacters.pop();
-document.getElementById("array-pop").innerHTML = goodCharacters;
+document.getElementById("pop").innerHTML = goodCharacters;
 
 // concat() - Merge two arrays together 
 const characters = goodCharacters.concat(badCharacters);
+document.getElementById("concat").innerHTML = characters;
 
 // Object Methods
 
 // log an array of the values of your object with Object.values()
 console.log(Object.values(dobby));
+document.getElementById("values").innerHTML = Object.values(dobby);
 
 // log an array of all the keys of your object with Object.keys()
 console.log(Object.keys(siriusBlack));
+document.getElementById("keys").innerHTML = Object.keys(siriusBlack);
 
 // Reduce an object’s editability with Object.freeze() 
 Object.freeze(severusSnape);
 severusSnape.house = "Gryffindor";
 console.log(severusSnape.house);
+document.getElementById("freeze").innerHTML = severusSnape.house;
+// can't render this code
+
 
 
